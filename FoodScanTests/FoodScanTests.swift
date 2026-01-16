b008d41938ac573c6d9d93b35d3a39e2b8408aa2
ishan-crd <ishangupta3121@gmail.com>
2026-01-16 18:59:52 +0700

    Initial Commit

---
+++ FoodScanTests/FoodScanTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodScanTests.swift
+//  FoodScanTests
+//
+//  Created by Ishan Gupta on 16/01/26.
+//
+
+import Testing
+@testable import FoodScan
+
+struct FoodScanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
